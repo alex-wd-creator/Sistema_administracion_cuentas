41f5b9de8ebc0e4e842b9eded5e2648a08e3fcc1
alex-wd-creator <aboukratalexis@gmail.com>
2026-03-31 09:33:41 -0300

    Add files via upload

---
+++ Sistema_administracion_cuentas.py
@@ -0,0 +1,18 @@
+print("*** Sistema de Administracion de cuentas ***")
+
+salir = False
+while not salir:
+    print(f"""Menu:
+          1. Crear Cuenta
+          2. Eliminar cuenta
+          3. Salir""")
+    opcion = int(input("escoja una opcion: "))
+    if opcion == 1:
+        print("Creando tu cuenta..\n")
+    elif opcion == 2:
+        print("Eliminando tu cuenta...\n")
+    elif opcion == 3:
+        print("Saliendo del sistema. Hasta pronto...\n")
+        salir = True
+    else:
+        print("Opcion invalida: Seleccione otra opción.")
